a93acfeb7001bb39fdbb69aa9671f6d96392972a
matcharr <mathis.charretier.42@gmail.com>
2022-05-02 13:33:06 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import PageHome from '@/components/PageHome.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: PageHome,
+  },
+];
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const forumApp = createApp(App);
+forumApp.use(router);
+forumApp.mount('#app');
